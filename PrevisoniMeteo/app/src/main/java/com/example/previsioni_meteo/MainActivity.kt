a4464a431b8cebb9cfe9921b301247342d32ccb3
Engine codegen <codegen@enginelabs.ai>
2024-09-17 20:05:23 +0000

    Backengine code update

---
+++ PrevisoniMeteo/app/src/main/java/com/example/previsioni_meteo/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.previsioni_meteo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        
+        // Qui aggiungeremo la logica per le previsioni meteo
+    }
+}
